8219d75372a4c2f4a2be8df3a52afbe13c49e9b1
Yasunori Morimoto <babooconex@gmail.com>
2014-11-15 01:13:05 +0900

    Emacsの設定ファイルを既存のものから github.com/purcell/emacs.d/ のものに変更。

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,200 +1,154 @@
-;; ~/.emacs.d/elisp ディレクトリをロードパスに追加する
-;; ただし、add-to-load-path関数を作成した場合は不要
-(add-to-list 'load-path "~/.emacs.d/elisp")
 
-;; load-path を追加する関数を定義
-(defun add-to-load-path (&rest paths)
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory
-	      (expand-file-name (concat user-emacs-directory path))))
-	(add-to-list 'load-path default-directory)
-	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-	    (normal-top-level-add-subdirs-to-load-path))))))
+;;; This file bootstraps the configuration, which is divided into
+;;; a number of other files.
 
-;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
-(add-to-load-path "elisp" "conf" "public_repos" "etc" "info" "el-get")
+(let ((minver "23.3"))
+  (when (version<= emacs-version "23.1")
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+(when (version<= emacs-version "24")
+  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
-;; http://coderepos.org/share/browser/lang/elisp/init-loader/init-loader.el
-(require 'init-loader)
-(init-loader-load "~/.emacs.d/conf") ; 設定ファイルがあるディレクトリを指定
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'init-benchmarking) ;; Measure startup time
 
-;; auto-installの設定
-(when (require 'auto-install nil t)
-  ;; インストールディレクトリを設定する 初期値は ~/.emacs.d/auto-install/
-  (setq auto-install-directory "~/.emacs.d/elisp/")
-  ;; EmacsWikiに登録されているelisp の名前を取得する
-  (auto-install-update-emacswiki-package-name t)
-  ;; 必要であればプロキシの設定を行う
-  ;; (setq url-proxy-services '(("http" . "localhost:8339")))
-  ;; install-elisp の関数を利用可能にする
-  (auto-install-compatibility-setup))
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+(defconst *is-a-mac* (eq system-type 'darwin))
 
-;; 2013-10-12 http://www.clear-code.com/blog/2012/3/20.html より追加。
-;;(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil t)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (end-of-buffer)
-    (eval-print-last-sexp)))
-;; レシピ置き場
-(add-to-list 'el-get-recipe-path
-             (concat (file-name-directory load-file-name) "/el-get/recipes"))
-;; 追加のレシピ置き場
-(add-to-list 'el-get-recipe-path
-             "~/.emacs.d/conf/el-get/local-recipes")
+;;----------------------------------------------------------------------------
+;; Bootstrap config
+;;----------------------------------------------------------------------------
+(require 'init-compat)
+(require 'init-utils)
+(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+(require 'init-elpa)      ;; Machinery for installing required packages
+(require 'init-exec-path) ;; Set up $PATH
 
-;; clパッケージを読み込む
-(require 'cl)
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-preload-local.el"
+;;----------------------------------------------------------------------------
+(require 'init-preload-local nil t)
 
-;;; P82-83 パスの設定
-(add-to-list 'exec-path "/opt/local/bin")
-(add-to-list 'exec-path "/usr/local/bin")
-(add-to-list 'exec-path "~/bin")
+;;----------------------------------------------------------------------------
+;; Load configs for specific features and modes
+;;----------------------------------------------------------------------------
 
-;;; P85 文字コードを指定する
-(set-language-environment "Japanese")
-(prefer-coding-system 'utf-8)
-;; P86 Windowsの場合のファイル名の設定
-(when (eq window-system 'w32)
-  (set-file-name-coding-system 'cp932)
-  (setq locale-coding-system 'cp932))
+(require-package 'wgrep)
+(require-package 'project-local-variables)
+(require-package 'diminish)
+(require-package 'scratch)
+(require-package 'mwe-log-commands)
 
-(setq initial-frame-alist
-      (append (list
-	       '(width . 81)
-	       '(height . 35)
-	       '(top . 40)
-	       '(left . 200)
-	       )
-	      initial-frame-alist))
-(setq default-frame-alist initial-frame-alist)
+(require 'init-frame-hooks)
+(require 'init-xterm)
+(require 'init-themes)
+(require 'init-osx-keys)
+(require 'init-gui-frames)
+(require 'init-proxies)
+(require 'init-dired)
+(require 'init-isearch)
+(require 'init-grep)
+(require 'init-uniquify)
+(require 'init-ibuffer)
+(require 'init-flycheck)
 
-;; 英語
-(set-face-attribute 'default nil
-      :family "Menlo" ;; font
-      :height 150)    ;; font size
+(require 'init-recentf)
+(require 'init-ido)
+(require 'init-hippie-expand)
+(require 'init-auto-complete)
+(require 'init-windows)
+(require 'init-sessions)
+(require 'init-fonts)
+(require 'init-mmm)
 
-;; 日本語
-(set-fontset-font
-  nil 'japanese-jisx0208
-  (font-spec :family "Hiragino_Kaku_Gothic_ProN")) ;; font
+(require 'init-editing-utils)
 
-;(set-frame-font "Ricty-12")
+(require 'init-vc)
+(require 'init-darcs)
+(require 'init-git)
+(require 'init-github)
 
-;; 半角と全角の比を1:2にしたければ
-(setq face-font-rescale-alist
-;;    '((".*Hiragino_Mincho_pro.*" . 1.2)))
-;;    '((".*Hiragino_Kaku_Gothic_ProN.*" . 1.2))) ;; Mac用フォント設定
-    '((".*Inconsolata.*" . 1.2))) ;; Mac用フォント設定
+(require 'init-compile)
+(require 'init-crontab)
+(require 'init-textile)
+(require 'init-markdown)
+(require 'init-csv)
+(require 'init-erlang)
+(require 'init-javascript)
+(require 'init-php)
+(require 'init-org)
+(require 'init-nxml)
+(require 'init-html)
+(require 'init-css)
+(require 'init-haml)
+(require 'init-python-mode)
+(require 'init-haskell)
+(require 'init-ruby-mode)
+(require 'init-rails)
+(require 'init-sql)
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
-(load-theme 'solarized-dark t) ;; または (load-theme 'solarized-light t)
+(require 'init-paredit)
+(require 'init-lisp)
+(require 'init-slime)
+(require 'init-clojure)
+(when (>= emacs-major-version 24)
+  (require 'init-clojure-cider))
+(require 'init-common-lisp)
 
-;; ターミナル以外はツールバー、スクロールバーを非表示
-(when window-system
-;; tool-barを非表示
-  (tool-bar-mode 0)
-;; scroll-barを非表示
-  (scroll-bar-mode 0))
+(when *spell-check-support-enabled*
+  (require 'init-spelling))
 
-;; スタートアップメッセージを非表示
-;(setq inhibit-startup-screen t)
+(require 'init-misc)
 
-;; タイトルバーにファイルのフルパスを表示
-(setq frame-title-format "%f")
+(require 'init-dash)
+(require 'init-ledger)
+;; Extra packages which don't require any configuration
 
-;; 行番号を常に表示する
-(global-linum-mode t)
+(require-package 'gnuplot)
+(require-package 'lua-mode)
+(require-package 'htmlize)
+(require-package 'dsvn)
+(when *is-a-mac*
+  (require-package 'osx-location))
+(require-package 'regex-tool)
 
-;; 背景色を黒に、前景色を緑系に変更
-;(set-background-color "black")
-;(set-foreground-color "#55ff55")
+;;----------------------------------------------------------------------------
+;; Allow access from emacsclient
+;;----------------------------------------------------------------------------
+(require 'server)
+(unless (server-running-p)
+  (server-start))
 
-;;; P100 現在行のハイライト
-(defface my-hl-line-face
-  ;; 背景がdarkならば背景色を紺に
-  '((((class color) (background dark))
-     (:background "NavyBlue" t))
-    ;; 背景がlightならば背景色を緑に
-    (((class color) (background light))
-     (:background "LightGoldenrodYellow" t))
-    (t (:bold t)))
-  "hl-line's my face")
-(setq hl-line-face 'my-hl-line-face)
-(global-hl-line-mode t)
 
-;; P101 括弧の対応関係のハイライト
-;; paren-mode：対応する括弧を強調して表示する
-(setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
-(show-paren-mode t) ; 有効化
-;; parenのスタイル: expressionは括弧内も強調表示
- (setq show-paren-style 'expression)
-;; フェイスを変更する
-(set-face-background 'show-paren-match-face nil)
-(set-face-underline-p 'show-paren-match-face "red")
+;;----------------------------------------------------------------------------
+;; Variables configured via the interactive 'customize' interface
+;;----------------------------------------------------------------------------
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
-;; リージョン内の行数と文字数をモードラインに表示する（範囲指定時のみ）
-;; http://d.hatena.ne.jp/sonota88/20110224/1298557375
-(defun count-lines-and-chars ()
-  (if mark-active
-      (format "%d lines,%d chars "
-              (count-lines (region-beginning) (region-end))
-              (- (region-end) (region-beginning)))
-      ;; これだとエコーエリアがチラつく
-      ;;(count-lines-region (region-beginning) (region-end))
-   ""))
 
-(add-to-list 'default-mode-line-format
-             '(:eval (count-lines-and-chars)))
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-local" containing personal settings
+;;----------------------------------------------------------------------------
+(when (file-exists-p (expand-file-name "init-local.el" user-emacs-directory))
+  (error "Please move init-local.el to ~/.emacs.d/lisp"))
+(require 'init-local nil t)
 
-;; C-mにnewline-and-indentを割り当てる。初期値はnewline
-(global-set-key (kbd "C-m") 'newline-and-indent)
 
-;; 折り返しトグルコマンド
-(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
-;; "C-t" でウィンドウを切り替える。初期値はtranspose-chars
-(define-key global-map (kbd "C-t") 'other-window)
+;;----------------------------------------------------------------------------
+;; Locales (setting them earlier in this file doesn't work in X)
+;;----------------------------------------------------------------------------
+(require 'init-locales)
 
-;; 入力されるキーシーケンスを置き換える
-;; ?\C-?はDELのキーシーケンス
-(keyboard-translate ?\C-h ?\C-?)
-;; 別のキーバインドにヘルプを割り当てる
-(define-key global-map (kbd "C-x q") 'help-command)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(show-paren-mode t))
-(put 'downcase-region 'disabled nil)
+(add-hook 'after-init-hook
+          (lambda ()
+            (message "init completed in %.2fms"
+                     (sanityinc/time-subtract-millis after-init-time before-init-time))))
 
-;; ruby-mode
-(autoload 'ruby-mode "ruby-mode"
-  "Mode for editing ruby source files" t)
-(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 
-;;; ruby-blockマイナーモード
-(require 'ruby-block)
-(ruby-block-mode t)
-(setq ruby-block-highlight-toggle t)
-;; 何もしない
-;;(setq ruby-block-highlight-toggle 'noghing)
-;; ミニバッファに表示
-;;(setq ruby-block-highlight-toggle 'minibuffer)
-;; オーバレイする
-;;(setq ruby-block-highlight-toggle 'overlay)
-;; ミニバッファに表示し, かつ, オーバレイする.
-(setq ruby-block-highlight-toggle t)
+(provide 'init)
 
-;; ruby-electric.el --- electric editing commands for ruby files
-(require 'ruby-electric)
-(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
-(setq ruby-electric-expand-delimiters-list nil)
-
-;; cua-modeの設定
-(cua-mode t) ; cua-modeをオン
-(setq cua-enable-cua-keys nil) ; CUAキーバインドを無効にする
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
